10a5c4536c727bf224f4eba94878429e9cdf2301
Uj Nana Soekmana <ujex23@gmail.com>
2023-07-24 02:24:30 +0700

    add : UI Dashboard

---
+++ resources/views/admin/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="footer text-center">
+    ©
+    <script>
+        document.write(new Date().getFullYear())
+    </script> Foxia <span class="d-none d-sm-inline-block"> - Crafted with <i
+            class="mdi mdi-heart text-danger"></i> by Themesbrand.</span>
+</footer>
